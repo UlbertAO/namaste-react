6dc9df263872738dfec9ac97a4be4b085a477ec7
ulbertAO <sp595993@gmail.com>
2024-04-03 14:19:23 +0530

    need of jsx/tsx

--- EP 1 Inception/App.js
+++ EP 1 Inception/App.js
@@ -3,6 +3,32 @@ const heading = React.createElement(
   { id: "heading" },
   "Hello World from React"
 ); // create h1 element
-console.log(heading);
+// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root")); // react need a place to render items
 root.render(heading); // add heading to root
+
+// in real scenarios we wil have nested elements in how can we create it in react
+
+// <div id="parent">
+//     <div id="child">
+//         <p></p>
+//     </div>
+// </div>
+//  react element is nothing but an object, when it is rendered into dom then it is converted to html
+const heading1 = React.createElement("h1", { id: "head" }, "content in h1");
+const heading2 = React.createElement("h2", { id: "head" }, "content in h2");
+const child1 = React.createElement("div", { id: "child" }, [
+  heading1,
+  heading2,
+]);
+const child2 = React.createElement("div", { id: "child" }, [
+  heading1,
+  heading2,
+]);
+
+const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+console.log(parent);
+root.render(parent);
+
+// here you can see for a simple struction react code looks horrible imagine for real application how it will look react is not making things easy right
+// that is why there exist jsx/tsx
